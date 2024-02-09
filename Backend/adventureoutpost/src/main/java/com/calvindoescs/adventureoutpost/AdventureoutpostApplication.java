2dd1eea333ac434ba4366389a85580baf9586cea
CalvinDoesCS <calvinntran.cs@gmail.com>
2024-02-08 23:38:03 -0500

    Init

---
+++ Backend/adventureoutpost/src/main/java/com/calvindoescs/adventureoutpost/AdventureoutpostApplication.java
@@ -0,0 +1,13 @@
+package com.calvindoescs.adventureoutpost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdventureoutpostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdventureoutpostApplication.class, args);
+	}
+
+}
---
+++ Backend/adventureoutpost/src/test/java/com/calvindoescs/adventureoutpost/AdventureoutpostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calvindoescs.adventureoutpost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdventureoutpostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
